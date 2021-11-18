98e7cbf4e5f89c4d32588b049b262b6b11079e6b
BolbGe <94233447+BolbGe@users.noreply.github.com>
2021-11-19 00:38:49 +0300

    Update DZ5.py

--- DZ5.py
+++ DZ5.py
@@ -3,8 +3,8 @@ s = int(input())
 mx = 0
 
 for i in range(1, s):
-    for j in range(1, s - i):
-        tmp = i // 2 * (1 - (i % 2)) + ((j + 1) // 2) * (1 - (j + 1) % 2)
+    for j in range(1, s - i + 1):
+        tmp = i // 2 * (1 - (i % 2)) + (j // 2) * (1 - j % 2)
         if tmp > mx:
             mx = tmp
             
